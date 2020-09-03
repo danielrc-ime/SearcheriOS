a9598eb4e65ea98f3a470957329abb3b0616c1e0
daniel_master <daniel.rodriguez@master.com.mx>
2020-09-03 10:01:25 -0500

    Initial Commit

---
+++ Searcher/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Searcher
+//
+//  Created by MacInnovacion on 03/09/20.
+//  Copyright © 2020 none. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
